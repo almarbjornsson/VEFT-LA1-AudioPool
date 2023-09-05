b45e7282b47b74c33e0633565cb5417bfea29187
AlbertGunnarsson <albertjaran25@gmail.com>
2023-09-05 17:01:02 +0000

    Some configurations and migrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Corrected line to add DbContext
+builder.Services.AddDbContext<AudioPoolDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ API/Repositories/AudioPoolDbContext.cs
@@ -0,0 +1,18 @@
+
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;  
+
+namespace API.Repositories
+{
+    public class AudioPoolDbContext : DbContext
+    {
+        public AudioPoolDbContext(DbContextOptions<AudioPoolDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Song> Songs { get; set; }
+    }
+}
